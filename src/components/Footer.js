42bb7553c419776cd4ab6866586d3da24793444b
Ahmadjon <79581200+ahmadjon-coder-creator@users.noreply.github.com>
2022-08-26 12:03:30 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
         <div class="footer-copyright">
           <div class="container">
           © {new Date().getFullYear()} Copyright Text
-          <a class="grey-text text-lighten-4 right" href="#!">REPO</a>
+          <a class="grey-text text-lighten-4 right" href="https://t.me/My_portf0lio">REPO</a>
           </div>
         </div>
       </footer>
